21f79571726f62b2a048a9bd8e029bd1477c9c75
Arkar Hein <arkerhein44@gmail.com>
2024-12-05 21:53:55 +0630

    error cleared

--- firebase/l11cdnjstodolist/app.js
+++ firebase/l11cdnjstodolist/app.js
@@ -4,164 +4,164 @@ const db = getFirestore();
 const dbRef = collection(db, "mytasks");
 
 
-// //UI
-// const getform = document.getElementById('form');
-// const gettextbox = document.getElementById('textbox');
-// const getul = document.querySelector('.list-group');
+//UI
+const getform = document.getElementById('form');
+const gettextbox = document.getElementById('textbox');
+const getul = document.querySelector('.list-group');
 
-// const tasks = [];
+let tasks = [];
 
-// getform.addEventListener('submit',(e)=>{
-//     //console.log('hay');
-//     addnew();
-//     e.preventDefault();
-// });
+getform.addEventListener('submit',(e)=>{
+    //console.log('hay');
+    addnew();
+    e.preventDefault();
+});
 
 
-// const getdatas = async()=>{
+const getdatas = async()=>{
 	
-// 	try{
-// 		// Method 1 need reload 
-// 		// const docSnap = await getDoc(dbRef);
-// 		// // console.log(docSnap);
+	try{
+		// Method 1 need reload 
+		// const docSnap = await getDoc(dbRef);
+		// // console.log(docSnap);
 
-// 		// docSnap.forEach(doc=>{
-// 		// 	// console.log(doc.data());
-// 		// 	// console.log(doc.id);
+		// docSnap.forEach(doc=>{
+		// 	// console.log(doc.data());
+		// 	// console.log(doc.id);
 
-// 		// 	let gettasks = doc.data();
-// 		// 		gettasks.id = doc.id;
-// 		// 		console.log(gettasks);
+		// 	let gettasks = doc.data();
+		// 		gettasks.id = doc.id;
+		// 		console.log(gettasks);
 			
-// 		// 	tasks.push(gettasks);
+		// 	tasks.push(gettasks);
 
-// 		// });
+		// });
 
-// 		// console.log(tasks);
+		// console.log(tasks);
 
-// 		//=> Method 2 (live)
-// 		await onSnapshot(dbRef,docSnap=>{
+		//=> Method 2 (live)
+		await onSnapshot(dbRef,docSnap=>{
 			
-// 			tasks = [];  // reset empty for clear previous pushed data
+			tasks = [];  // reset empty for clear previous pushed data
 
-// 			docSnap.forEach(doc=>{
-// 				// console.log(doc.data());
-// 				// console.log(doc.id);
+			docSnap.forEach(doc=>{
+				// console.log(doc.data());
+				// console.log(doc.id);
 	
-// 				let gettasks = doc.data();
-// 					gettasks.id = doc.id;
-// 					// console.log(gettasks);
+				let gettasks = doc.data();
+					gettasks.id = doc.id;
+					// console.log(gettasks);
 				
-// 				tasks.push(gettasks);
+				tasks.push(gettasks);
 	
-// 			});
-// 			// console.log(gettasks);
-// 			showtaskstodom(tasks);
-// 		});
+			});
+			// console.log(gettasks);
+			showtaskstodom(tasks);
+		});
 
-// 	}catch(error){
-// 		console.log("Get Data Error : ", err);
-// 	}
-// };
+	}catch(error){
+		console.log("Get Data Error : ", err);
+	}
+};
 
-// getdatas();
+getdatas();
 
-// const showtaskstodom = (taskarrs)=>{
-// 	getul.innerHTML = '';
+const showtaskstodom = (taskarrs)=>{
+	getul.innerHTML = '';
 
-// 	taskarrs.forEach(task=>{
-// 		const li = document.createElement('li');
-// 		li.appendChild(document.createTextNode(task.todo));
-// 		li.id = task.id;
-// 		li.classList.add("list-group-item");
+	taskarrs.forEach(task=>{
+		const li = document.createElement('li');
+		li.appendChild(document.createTextNode(task.todo));
+		li.id = task.id;
+		li.classList.add("list-group-item");
 
-// 		li.innerHTML += `
-// 				<div class="action">
-// 					<button type="button" class="edit-btn"><i class="fas fa-edit"></i></button>
-// 					<button type="button" class="delete-btn"><i class="fas fa-trash-alt"></i></button>
-// 				</div>
-// 		`;
+		li.innerHTML += `
+				<div class="action">
+					<button type="button" class="edit-btn"><i class="fas fa-edit"></i></button>
+					<button type="button" class="delete-btn"><i class="fas fa-trash-alt"></i></button>
+				</div>
+		`;
 
-// 		console.log(li);
+		console.log(li);
 
-// 		getul.appendChild(li);
-// 	})
-// }
+		getul.appendChild(li);
+	})
+}
 
 
-// async function addnew() {
-// 	const todotext = gettextbox.value;
-// 	// console.log(todotext);
+async function addnew() {
+	const todotext = gettextbox.value;
+	// console.log(todotext);
 
-// 	if(gettextbox.getAttribute('task-id')){
-// 		// Update data
+	if(gettextbox.getAttribute('task-id')){
+		// Update data
 
-// 		try{
-// 			await updateDoc(dbRef,{
-// 				todo:todotext,
-// 				done:false
-// 			});
+		try{
+			await updateDoc(dbRef,{
+				todo:todotext,
+				done:false
+			});
 
-// 			gettextbox.value="";
-// 			gettextbox.focus();
-// 			gettextbox.removeAttribute('task-id');
+			gettextbox.value="";
+			gettextbox.focus();
+			gettextbox.removeAttribute('task-id');
 			 
-// 		}catch(error){
-// 			console.log("Update Data Error : " , error)		;
-// 		}
+		}catch(error){
+			console.log("Update Data Error : " , error)		;
+		}
 
-// 	}else{
-// 		// Create Data
+	}else{
+		// Create Data
 
-// 		try{
-// 			await addDoc(dbRef,{
-// 				todo:todotext,
-// 				done: false
-// 			});
+		try{
+			await addDoc(dbRef,{
+				todo:todotext,
+				done: false
+			});
 
-// 			gettextbox.value="";
-// 			gettextbox.focus();
-// 		}catch(error){
-// 			console.log("Create Data Error : " , error)
-// 		}
+			gettextbox.value="";
+			gettextbox.focus();
+		}catch(error){
+			console.log("Create Data Error : " , error)
+		}
 
-// 	}
-// }
+	}
+}
 
-// getul.addEventListener('click', getclickedli);
+getul.addEventListener('click', getclickedli);
 
-// function getclickedli(e){
-// 	// console.log(e.target);
-// 	// console.log(e.target.closest("li"));
-// 	// console.log(e.target.closest("li").getAttribute('id'));
+function getclickedli(e){
+	// console.log(e.target);
+	// console.log(e.target.closest("li"));
+	// console.log(e.target.closest("li").getAttribute('id'));
 
-// 	// console.log(e.target.className);
-// 	// console.log(e.target.closest("button").className);
+	// console.log(e.target.className);
+	// console.log(e.target.closest("button").className);
 
-// 	const getid = e.target.closest("li").getAttribute('id');
+	const getid = e.target.closest("li").getAttribute('id');
 
-// 	if(e.target.closest("button").className === "edit-btn"){
-// 		edittask(getid);
-// 	}else if(e.target.closest("button").className === "delete-btn"){
-// 		deletetask(getid);
-// 	}else{
-// 		donetask(getid);
-// 	}
+	if(e.target.closest("button").className === "edit-btn"){
+		edittasks(getid);
+	}else if(e.target.closest("button").className === "delete-btn"){
+		deletetask(getid);
+	}else{
+		donetask(getid);
+	}
 
-// 	// if(e.target.)
-// }
+	// if(e.target.)
+}
 
-// function edittasks(id){
-// 	console.log("edit tasks id = ",id);
-// }
+function edittasks(id){
+	console.log("edit tasks id = ",id);
+}
 
-// function deletetask(id){
-// 	console.log("delete tasks id = ",id);
-// }
+function deletetask(id){
+	console.log("delete tasks id = ",id);
+}
 
-// async function donetask(id){
-// 	console.log("done tasks id = ",id);
-// }
+async function donetask(id){
+	console.log("done tasks id = ",id);
+}
 
 // import { } from 'https://www.gstatic.com/firebasejs/11.0.2/firebase-SERVICE.js'
 
